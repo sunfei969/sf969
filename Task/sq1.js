9be1133c9decdff038e71297c5bb98e96978d673
sunfei969 <76869401+sunfei969@users.noreply.github.com>
2021-08-20 17:10:50 +0800

    Update sq1.js

--- Task/sq1.js
+++ Task/sq1.js
@@ -522,7 +522,7 @@ function dailysign() {
             "User-Agent" : "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 AliApp(shuqi/4.3.1.0) WindVane/8.6.1 Shuqi (iPhone11,6__shuqi__v4.3.1.0) 1242x2688 Winding(WV_2) WK",
             "Referer" : "https://render-web.shuqireader.com/render/sq-welfare/page/welfare_page_v2/?serviceWorkerOn=1&stopPullRefresh=1&upf=20559&from=ShuqiTab&sdk=14.0.1&utdid=YFHvkSdla5UDAO0ygGI6DM3%2F&ustatus=1&first_placeid=111111&net_env=2g&placeid=111111&user_id=2038268844&sn=C1F3B9E12724162869F18F9CCB7E4CE361047973&umidtoken=0W9LQQNLOiq%2ByjV7Vyfw4qXcGjitXJk%2F&msv=8.0.0&brand=Apple&imei=07CBD4AFEF888A4F58D65BD337D482ECE9566F5D&skinVersionPrefix=1&appVer=4.3.1.0&skinActiveColor=0F9970&manufacturer=Apple&session=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMDM4MjY4ODQ0IiwidXRkaWQiOiIiLCJpbWVpIjoiMDdDQkQ0QUZFRjg4OEE0RjU4RDY1QkQzMzdENDgyRUNFOTU2NkY1RCIsInNuIjoiQzFGM0I5RTEyNzI0MTYyODY5RjE4RjlDQ0I3RTRDRTM2MTA0Nzk3MyIsImV4cCI6MTYyOTc3NTA5OSwidXNlcklkIjoiMjAzODI2ODg0NCIsImlhdCI6MTYyOTI1NjY5OSwib2FpZCI6IiIsInBsYXRmb3JtIjoiaU9TIn0.XrAWc9aaQlh_z_vbu9UNHWfuEUFa1dho_ITgAI8ZM2tBJIoDmA71KxZLwKDdr56X0CmHtF6L77JDDPX6Nvqofg&skinColor=23B383&platform=iOS&ver=210111&mod=iPhoneXMAX&statusBarHeight=44.000000&skinVersion=1&wh=1242x2688&soft_id=21&utype=vip&skinId=999&idfa=FA83D0D2-0895-482A-8B1B-48011F454733",
             "Accept-Language" : "zh-cn"
-            'Origin' : 'https://render-web.shuqireader.com',
+            Origin : https://render-web.shuqireader.com,
             "Accept-Encoding" : "gzip, deflate, br",
             "Connection" : "keep-alive",
             "Content-Type" : "application/x-www-form-urlencoded",
